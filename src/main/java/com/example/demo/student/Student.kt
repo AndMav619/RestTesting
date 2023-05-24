6b59649aa5707696ddd4732f6a3618f63f3fa7e8
AndMav619 <131683726+AndMav619@users.noreply.github.com>
2023-05-24 02:32:08 +0200

    no message

--- src/main/java/com/example/demo/student/Student.kt
+++ src/main/java/com/example/demo/student/Student.kt
@@ -11,27 +11,27 @@ class Student {
     @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     var id: Long? = null
+
     var name: String? = null
     var email: String? = null
     var dob: LocalDate? = null
-
     @Transient
     private var age: Int? = null
 
     constructor()
-    constructor(id: Long?,
-                name: String?,
-                email: String?,
-                dob: LocalDate?) {
+    constructor(id: Long,
+                name: String,
+                email: String,
+                dob: LocalDate) {
         this.id = id
         this.name = name
         this.email = email
         this.dob = dob
     }
 
-    constructor(name: String?,
-                email: String?,
-                dob: LocalDate?) {
+    constructor(name: String,
+                email: String,
+                dob: LocalDate) {
         this.name = name
         this.email = email
         this.dob = dob
@@ -41,7 +41,7 @@ class Student {
         return Period.between(dob, LocalDate.now()).years
     }
 
-    fun setAge(age: Int?) {
+    fun setAge(age: Int) {
         this.age = age
     }
 
